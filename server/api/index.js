f127f084a261349272198e2e839253c714c336d3
Breea Toomey <breeatoomey@gmail.com>
2024-05-21 12:52:55 -0700

    testing

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Backend Deployed successfully"));
+app.get("/", (req, res) => res.send("Testing"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
