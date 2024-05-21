5af5d2cc0b4ee987f90acf5e6811e021af07f3de
Breea Toomey <breeatoomey@gmail.com>
2024-05-21 15:44:50 -0700

    attempting to fix cors error

--- server/api/index.js
+++ server/api/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
+const app = express();
 
-const data = '{ "name": "Alice", "age": 30, "city": "New York", "skills": ["JavaScript", "Python", "Java"], "isActive": true }';
+const data = '{ "name": "Alice", "age": 30, "email": "alice@gmail.com", "city": "New York", "skills": ["JavaScript", "Python", "Java"], "isActive": true }';
 
 app.use(cors());
-app.get("/", (req, res) => res.send(data));
+
+app.get("/", (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', `true`)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.send(data)
+});
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
