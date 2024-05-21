d57edf6a571e8e36f0f992b6927bebd3ec63e0e8
DenaliTonn <dtonn1@lion.lmu.edu>
2024-05-21 13:27:07 -0700

    Implemented Axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const apiCall = () => {
+    axios.get("http://localhost:3000").then((data) => {
+      console.log(data);
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={apiCall}>Make API Call</button>
       </header>
     </div>
   );
--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+console.log(app);
 app.get("/", (req, res) => res.send("Testing"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
