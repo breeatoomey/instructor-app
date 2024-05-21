b5a099869d5d732332925526e20a350b77e56085
DenaliTonn <dtonn1@lion.lmu.edu>
2024-05-21 14:40:06 -0700

    Implemented CORS

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 console.log(app);
+
+app.use(cors());
 app.get("/", (req, res) => res.send("Testing"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
