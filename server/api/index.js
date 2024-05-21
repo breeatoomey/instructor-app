7d94ef0986664b0df5a152a6d8e5bdab0a71ab15
Breea Toomey <breeatoomey@gmail.com>
2024-05-21 12:41:23 -0700

    trying

--- server/api/index.js
+++ server/api/index.js
@@ -1,15 +1,8 @@
-import express from 'express';
-import cors from 'cors';
+const express = require("express");
 const app = express();
 
-app.use(cors({
-    origin: 'https://instructor-cfb29vz51-breea-toomeys-projects.vercel.app'
-}));
+app.get("/", (req, res) => res.send("Backend Deployed"));
 
-app.get('/', (req, res) => {
-      res.send('Backend server is deployed.')
-})
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
-app.listen(8080, () => {
-      console.log('server listening on port 8080')
-})
+module.exports = app;
