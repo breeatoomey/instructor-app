5228c3b2aec449b378aec7d21d3c430b2e12ca34
Breea Toomey <breeatoomey@gmail.com>
2024-05-21 12:26:56 -0700

    new client

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,22 @@
-// import logo from './logo.svg';
-import axios from 'axios';
+import logo from './logo.svg';
 import './App.css';
-
-//data will be the string we send from our server
-const apiCall = () => {
-  axios.get('https://backend-58xtxw1gn-breea-toomeys-projects.vercel.app').then((data) => {
-    //this console.log will be in our frontend console
-    console.log(data)
-  })
-}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
-        <p>Test Deployment 2</p>
-
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
