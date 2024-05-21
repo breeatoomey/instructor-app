639e17cd48914ed8781566cf33c40fdec3e1c375
Breea Toomey <breeatoomey@gmail.com>
2024-05-21 15:30:52 -0700

    added json data

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-console.log(app);
+
+const data = '{ "name": "Alice", "age": 30, "city": "New York", "skills": ["JavaScript", "Python", "Java"], "isActive": true }';
 
 app.use(cors());
-app.get("/", (req, res) => res.send("Backend deployment successful."));
+app.get("/", (req, res) => res.send(data));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
