27ddc6804ad57509017a33f434783db6312f7a7a
djacosta-01 <djacosta.07@gmail.com>
2024-06-18 21:52:51 -0700

    able to successfully parse user input and post info to graph server

--- client/src/Components/ClassPage/Pages/EditKnowledgeGraph.jsx
+++ client/src/Components/ClassPage/Pages/EditKnowledgeGraph.jsx
@@ -24,21 +24,50 @@ const EditKnowledgeGraph = () => {
     console.log('edgesFromInput', edgesFromInput)
   }
 
+  const updateAndPostGraph = async (nodes, edges) => {
+    try {
+      const response = await fetch('http://localhost:5000/store-kg', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          nodes: nodes,
+          edges: edges,
+          class_name: 'CS-101',
+          graph_name: 'class_avg_kg',
+        }),
+      })
+      const result = await response.json()
+      console.log('Success:', result)
+    } catch (error) {
+      console.error('error', error)
+    }
+  }
+
   const submitForm = event => {
     event.preventDefault()
     console.log(input)
     const lines = input.split('\n').filter(line => line !== '')
     console.log('lines', lines)
     parseInput(lines)
-    /**
-     * call api here to update the knowledge graph
-     * just hardcode the class name/id for now
-     * after api call:
-     * reset relevant states
-     * construct the graph if one if not already constructed
-     * if constructed, update the graph
-     */
+    console.log('nodes', nodes)
+    console.log('edges', edges)
+    updateAndPostGraph(nodes, edges)
+    // setNodes([])
+    // setEdges([])
   }
+
+  /**
+   * call api here to update the knowledge graph
+   * just hardcode the class name/id for now
+   * after api call:
+   * reset relevant states
+   * construct the graph if one exists in datastore
+   * if constructed, update the graph
+   */
+
+  // fetch graph here??
 
   return (
     <div className="editKnowledgeGraph">
