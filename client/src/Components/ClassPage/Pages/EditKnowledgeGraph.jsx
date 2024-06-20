7b2cc104dc7364622bfb54edcb82135f10d4a57b
djacosta-01 <djacosta.07@gmail.com>
2024-06-20 12:58:46 -0700

    trying to catch edge cases I missed when parsing user input for the graph

--- client/src/Components/ClassPage/Pages/EditKnowledgeGraph.jsx
+++ client/src/Components/ClassPage/Pages/EditKnowledgeGraph.jsx
@@ -1,74 +1,72 @@
-import TextField from '@mui/material/TextField'
-import { useState } from 'react'
-import { useEffect } from 'react'
+import { Box, Paper, TextField } from '@mui/material'
+import { useState, useEffect } from 'react'
 
 const EditKnowledgeGraph = () => {
   const [input, setInput] = useState('== Level 1 ==\nA --> B\nA --> C\n== Level 2 ==\nB --> C')
   const [nodes, setNodes] = useState([])
   const [edges, setEdges] = useState([])
 
-  const parseInput = input => {
-    const nodesFromInput = new Set()
-    const edgesFromInput = new Set()
-    input.forEach(line => {
-      if (!line.includes('==')) {
-        const edge = line.split(' --> ')
-        nodesFromInput.add(edge[0])
-        nodesFromInput.add(edge[1])
-        edgesFromInput.add(edge)
-      }
-    })
-    setNodes(Array.from(nodesFromInput))
-    setEdges(Array.from(edgesFromInput))
-    console.log('nodesFromInput', nodesFromInput)
-    console.log('edgesFromInput', edgesFromInput)
-  }
-
-  const updateAndPostGraph = async (nodes, edges) => {
-    try {
-      const response = await fetch('http://localhost:5000/store-kg', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          nodes: nodes,
-          edges: edges,
-          class_name: 'CS-101',
-          graph_name: 'class_avg_kg',
-        }),
-      })
-      const result = await response.json()
-      console.log('Success:', result)
-    } catch (error) {
-      console.error('error', error)
-    }
-  }
-
   const submitForm = event => {
     event.preventDefault()
     console.log(input)
-    const lines = input.split('\n').filter(line => line !== '')
-    console.log('lines', lines)
-    parseInput(lines)
+    const nodesFromInput = new Set()
+    const edgesFromInput = new Set()
+    // parsing input
+    input
+      .split('\n')
+      .filter(line => line !== '' && line !== ' ')
+      .forEach(line => {
+        if (!line.includes('==')) {
+          const edge = line
+            .trim()
+            .split(' --> ')
+            .filter(edge => edge !== ' ')
+          nodesFromInput.add(edge[0])
+          nodesFromInput.add(edge[1])
+          edgesFromInput.add(edge)
+        }
+      })
+    console.log('nodesFromInput', nodesFromInput)
+    console.log('edgesFromInput', edgesFromInput)
+    setNodes(Array.from(nodesFromInput))
+    setEdges(Array.from(edgesFromInput))
     console.log('nodes', nodes)
     console.log('edges', edges)
-    updateAndPostGraph(nodes, edges)
-    // setNodes([])
-    // setEdges([])
   }
+  // posting graph to knowledge graph datastore
+  useEffect(() => {
+    const updateAndPostGraph = async (nodes, edges) => {
+      if (nodes.length > 0 && edges.length > 0) {
+        try {
+          const response = await fetch('http://localhost:5000/store-kg', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              nodes: nodes,
+              edges: edges,
+              class_name: 'CS-101',
+              graph_name: 'class_avg_kg',
+            }),
+          })
+          const result = await response.json()
+          console.log('Success:', result)
+        } catch (error) {
+          console.error('error', error)
+        }
+      }
+    }
+    updateAndPostGraph(nodes, edges)
+    // resetting states
+    if (nodes.length > 0 && edges.length > 0) {
+      setNodes([])
+      setEdges([])
+    }
+  }, [nodes, edges])
 
-  /**
-   * call api here to update the knowledge graph
-   * just hardcode the class name/id for now
-   * after api call:
-   * reset relevant states
-   * construct the graph if one exists in datastore
-   * if constructed, update the graph
-   */
-
-  // fetch graph here??
-
+  console.log('nodes', nodes)
+  console.log('edges', edges)
   return (
     <div className="editKnowledgeGraph">
       <h1> Edit Knowledge Graph </h1>
@@ -82,6 +80,7 @@ const EditKnowledgeGraph = () => {
         />
         <input type="submit" value="Update" />
       </form>
+      <div id="graph">graph goes here</div>
     </div>
   )
 }
