f13322936462f05a4962d0d6ec2ee0ff4b94b693
djacosta-01 <djacosta.07@gmail.com>
2024-06-25 16:24:45 -0700

    finally got graph to properly display. will work on making code for more generalizable since I hardcoded it to work for a graph of 3 levels

--- client/src/Components/ClassPage/Pages/EditKnowledgeGraph.jsx
+++ client/src/Components/ClassPage/Pages/EditKnowledgeGraph.jsx
@@ -1,44 +1,41 @@
 import { TextField, Stack } from '@mui/material'
 import { useState, useEffect } from 'react'
-import ReactFlow from 'reactflow'
-import 'reactflow/dist/style.css'
-
 import KnowledgeGraph from './Graph'
+
 const EditKnowledgeGraph = () => {
   const [input, setInput] = useState(
     '== Level 1 ==\nTypes --> Variables\nExpressions --> Conditionals\nScope --> Lists\n== Level 2 ==\nVariables --> Iteration\nConditionals --> Iteration\nLists --> Random Access\n== Level 3 ==\nIteration\nRandom Access\n'
   )
   const [nodesFromInput, setNodesFromInput] = useState([])
   const [edgesFromInput, setEdgesFromInput] = useState([])
-  const [nodesForDisplay, setNodesForDisplay] = useState([])
-  const [edgesForDisplay, setEdgesForDisplay] = useState([])
-  // const [nodesForDisplay, setNodesForDisplay] = useState(initialNodes)
-  // const [edgesForDisplay, setEdgesForDisplay] = useState(initialEdges)
 
   /**
    * Parses the user's input and returns an array of arrays, where each sub-array represents a level in the graph.
    * Each element in a sub-array is a parent node of the elements in the subsequent sub-arrays.
-   * [EXAMPLE INPUT]:
+   *
+   * Example Input:
    *    ==Level 1==
    *    A --> B
    *    C --> D
    *    ==Level 2==
    *    B --> E
    *    D --> F
-   *   ==Level 3==
+   *    ==Level 3==
    *    E
    *    F
    *
-   * [EXAMPLE RESULT]:
+   * Example Result:
    *   [[A, C], [B, D], [E, F]]
    *
+   * @param {string} input - The user's input string representing the levels and nodes.
+   * @returns {Array<Array<string>>} - An array of arrays, where each sub-array contains the nodes at each level.
    */
   const getNodesAtEachLevel = input => {
     const regex = /==\s*Level\s*\d+\s*==/
     return input
       .split(regex)
       .map(line => line.trim().split('\n'))
-      .filter(arr => !arr.includes(''))
+      .filter(arr => arr.length && !arr.includes(''))
       .map(arr => arr.map(line => line.split(' --> ')))
       .map(level => level.map((nodes, index, level) => level[index][0]))
   }
@@ -64,26 +61,12 @@ const EditKnowledgeGraph = () => {
       })
     return [nodes, edges]
   }
+
   const submitForm = event => {
     event.preventDefault()
     const [nodes, edges] = parseInput(input)
     setNodesFromInput(Array.from(nodes))
     setEdgesFromInput(Array.from(edges))
-
-    // FIXME: Maybe retrieve the info of nodes and edges from server once the user submits the form and graph is added??
-    setNodesForDisplay(
-      nodesFromInput.map(node => {
-        return { id: node, data: { label: node }, position: { x: 100, y: 100 } }
-      })
-    )
-    setEdgesForDisplay(
-      edgesFromInput.map(edges => {
-        const id = `e${edges[0]}-${edges[1]}`
-        const source = `${edges[0]}`
-        const target = `${edges[1]}`
-        return { id, source, target, animated: true }
-      })
-    )
   }
 
   // posting graph to knowledge graph datastore
@@ -119,13 +102,15 @@ const EditKnowledgeGraph = () => {
     }
   }, [nodesFromInput, edgesFromInput])
 
-  const nodesToDisplay = getNodesAtEachLevel(input).map(level => Array.from(new Set(level)))
-  // console.log(nodesToDisplay)
+  const parsedNodes = getNodesAtEachLevel(input).map(level => Array.from(new Set(level)))
+  console.log('NODES TO DISPLAY')
+  console.log(parsedNodes)
 
   return (
     <div className="editKnowledgeGraph">
       <h1> Edit Knowledge Graph </h1>
       <Stack direction="row" justifyContent="center" spacing={10}>
+        {/* add Info icon for user input */}
         <form id="graph-input" onSubmit={submitForm}>
           <TextField
             label="Knowledge Graph"
@@ -136,8 +121,7 @@ const EditKnowledgeGraph = () => {
           />
           <input type="submit" value="Update" />
         </form>
-
-        <KnowledgeGraph />
+        <KnowledgeGraph nodesInLevels={parsedNodes ? parsedNodes : ''} />
       </Stack>
     </div>
   )
--- client/src/Components/ClassPage/Pages/Graph.jsx
+++ client/src/Components/ClassPage/Pages/Graph.jsx
@@ -2,25 +2,49 @@ import { useState, useEffect } from 'react'
 import ReactFlow from 'reactflow'
 import 'reactflow/dist/style.css'
 
-const KnowledgeGraph = () => {
+const KnowledgeGraph = ({ nodesInLevels }) => {
   const [nodes, setNodes] = useState([])
   const [edges, setEdges] = useState([])
-  //   const [nodes, setNodes] = useState(initialNodes)
-  //   const [edges, setEdges] = useState(initialEdges)
   const [responseCode, setResponseCode] = useState(404)
 
   const formatNodes = nodes => {
-    let xPos = 100
-    let yPos = 100
-    return nodes.map(node => {
-      xPos += 100
-      yPos += 50
-      const id = node
-      //   const type = 'input'
-      const data = { label: node }
-      const position = { x: xPos, y: yPos }
-      return { id, data, position }
-    })
+    // FIXME: hardcoding positions for now
+    let xPosLevel1 = 100
+    let yPosLevel1 = 25
+    let xPosLevel2 = 100
+    let yPosLevel2 = 100
+    let xPosLevel3 = 100
+    let yPosLevel3 = 175
+
+    return nodesInLevels.flatMap((level, levelIndex) =>
+      level.map((node, index) => {
+        // at root level
+        if (levelIndex === 0) {
+          xPosLevel1 += (xPosLevel1 % 2) + 200
+          const id = node
+          const type = 'input'
+          const data = { label: node }
+          const position = { x: xPosLevel1, y: yPosLevel1 }
+          return { id, type, data, position }
+        }
+        // xPos = 100 + 200 * levelIndex
+        // at last level
+        if (levelIndex === nodesInLevels.length - 1) {
+          xPosLevel3 += (xPosLevel3 % 2) + 200
+          const id = node
+          const type = 'output'
+          const data = { label: node }
+          const position = { x: xPosLevel3, y: yPosLevel3 + 250 }
+          return { id, type, data, position }
+        }
+        // at other levels
+        xPosLevel2 += (xPosLevel2 % 2) + 200
+        const id = node
+        const data = { label: node }
+        const position = { x: xPosLevel2, y: yPosLevel2 + 125 }
+        return { id, data, position }
+      })
+    )
   }
 
   const formatEdges = edges => {
@@ -35,7 +59,7 @@ const KnowledgeGraph = () => {
   useEffect(() => {
     const fetchGraph = async () => {
       try {
-        const response = await fetch('http://localhost:5000/classes/CS-101/average-graph')
+        const response = await fetch('http://localhost:5000/classes/CS-101/average-graph') // hardcoding class name for now
         setResponseCode(response.status)
         const data = await response.json()
         setNodes(formatNodes(data['nodes']))
@@ -48,17 +72,20 @@ const KnowledgeGraph = () => {
     fetchGraph()
   }, [])
 
-  console.log('NODES')
+  console.log('nodes')
   console.log(nodes)
-  console.log('EDGES')
+  console.log('edges')
   console.log(edges)
-  if (!nodes || !edges) {
-    return <div>Loading...</div>
-  }
   return (
-    <div id="better-graph" style={{ backgroundColor: 'aquamarine', height: 750, width: 750 }}>
-      <h3>React Flow Graph</h3>
-      <ReactFlow nodes={nodes} edges={edges} fitView />
+    <div id="knowledge-graph" style={{ height: 750, width: 750 }}>
+      {responseCode === 404 ? (
+        <h1 id="error">Graph not found</h1>
+      ) : (
+        <>
+          <h2>Class Graph</h2>
+          <ReactFlow nodes={nodes} edges={edges} fitView />
+        </>
+      )}
     </div>
   )
 }
